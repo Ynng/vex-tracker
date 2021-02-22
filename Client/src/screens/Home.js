716ea57c5273bb6726add206e0d46cd3cb571153
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2021-02-22 13:51:02 -0500

    attempting lazy loading

--- Client/src/screens/Home.js
+++ Client/src/screens/Home.js
@@ -10,28 +10,40 @@ let randomColor = () => {
 };
 
 function Home() {
-  const [data, setData] = useState({});
+  const [data, setData] = useState([]);
+  const [maxmin, setmaxmin] = useState({});
+  const [colors, setColors] = useState({});
   const [teams, setTeams] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [scrollTop, setScrollTop] = useState(0);
 
   let processData = (data) => {
-    let len = data.time.length;
+    let days = data.time.length;
     let newData = [];
-    let newTeams = Object.keys(data.teams);
-    setTeams(newTeams);
-    for (let i = 0; i < len; i++) {
+    let teams = Object.keys(data.teams);
+    let len = teams.length;
+    let newTeams = [];
+    let maxmin = {};
+    let colors = {};
+    for (let i = 0; i < days; i++) {
       newData[i] = {};
       newData[i]["time"] = data.time[i];
-      newTeams.forEach((key) => {
-        if(!data.teams[key][i]){
-          console.log(`${key} null`);
-          data.teams[key][i] = newTeams.length + 1;
-        }
-        newData[i][key] = data.teams[key][i];
+      teams.forEach((team, idx) => {
+        let rank = data.teams[team][i];
+        if (!rank) rank = teams.length + 1;
+        if (!maxmin[team]) maxmin[team] = { max: 0, min: len };
+        maxmin[team].max = Math.max(maxmin[team].max, rank);
+        maxmin[team].min = Math.min(maxmin[team].min, rank);
+        newData[i][team] = rank;
+        if (i === days - 1) newTeams[rank] = team;
+        if (i === days - 1) colors[team] = randomColor();
       });
     }
+    setmaxmin(maxmin);
+    setTeams(newTeams);
     setData(newData);
+    setColors(colors);
     console.log(newData);
   };
 
@@ -53,39 +65,68 @@ function Home() {
       .finally(() => {
         setLoading(false);
       });
+
+    let myScrollTop = 0;
+    //Scrolling
+    function onScroll() {
+      let currentPosition = window.pageYOffset;
+      if (Math.abs(currentPosition - myScrollTop) > 100) {
+        myScrollTop = currentPosition <= 0 ? 0 : currentPosition;
+        setScrollTop(myScrollTop);
+      }
+    }
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
   }, []);
 
   if (loading) return "Loading....";
   if (error) return "Error!";
 
   return (
-    <LineChart
-      width={data.length * 50}
-      height={teams.length * 20}
-      data={data}
-      margin={{
-        top: 5,
-        right: 30,
-        left: 20,
-        bottom: 5
-      }}
-    >
-      {teams.map((item) => {
-        return (
-          <Line
-            isAnimationActive={false}
-            type="monotone"
-            dataKey={item}
-            stroke={randomColor()}
-            strokeWidth={2}
-            key={item}
-          />
-        );
-      })}
-      <YAxis type={"number"} orientation={"right"} reversed />
-      <XAxis dataKey="time" />
-      {/* <Tooltip /> */}
-    </LineChart>
+    <>
+      <LineChart
+        width={data.length * 50}
+        height={teams.length * 20}
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        {teams.map((item, idx) => {
+          if (
+            maxmin[item].min >
+            (scrollTop + window.innerHeight + window.innerHeight) / 20
+          )
+            return null;
+          if (maxmin[item].max < (scrollTop - window.innerHeight) / 20)
+            return null;
+          return (
+            <Line
+              isAnimationActive={false}
+              type="monotone"
+              dataKey={item}
+              stroke={colors[item]}
+              strokeWidth={2}
+              key={item}
+            />
+          );
+        })}
+        <YAxis
+          type={"number"}
+          orientation={"right"}
+          reversed
+          interval={1}
+          domain={[0, teams.length]}
+        />
+        <XAxis dataKey="time" />
+        {/* <Tooltip /> */}
+      </LineChart>
+    </>
   );
 }
 export default Home;
