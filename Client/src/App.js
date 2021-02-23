30accaf427391690707e80d531708757cdd525a3
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2021-02-23 01:12:02 -0500

    minor

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,9 +3,19 @@ import "./App.css";
 import { useRef, useState, useEffect, Component } from "react";
 import Canvas from "react-responsive-canvas";
 import { Route, Switch } from "react-router-dom";
+import ReactGA from "react-ga";
 import Home from "./screens/Home";
 
+function initializeReactGA() {
+  ReactGA.initialize("G-7Z286BH7JF");
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
+
 function App() {
+  useEffect(() => {
+    initializeReactGA();
+  }, []);
+  
   return (
     <Switch>
       <Route path="/" component={Home} />
